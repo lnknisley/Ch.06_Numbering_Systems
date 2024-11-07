8b8bba5048317c12b595b3b53b341a9c97a34bdb
lnknisley <andthepandabomb@gmail.com>
2024-11-07 14:52:57 -0600

    I did it all

--- 6.0_Jedi_Training.py
+++ 6.0_Jedi_Training.py
@@ -1,23 +1,23 @@
 '''
-6.0 Jedi Training (15pts)  Name:________________
+6.0 Jedi Training (15pts)  Name: Logan Knisley
 
 
 1.) Convert Binary 101010 to Hexadecimal. (1pt)
-
+2A
 2.) Convert Binary 11101001 to Octal. (1pt)
-
+231 347
 3.) Convert Hexadecimal FC to Binary. (1pt)
-
+11111100
 4.) Convert Hexadecimal 1F to Decimal. (1pt)
-
+31
 5.) Convert Hexadecimal #FAAFBD to RGB. (1pt)
-
+(250, 175, 190)
 6.) Convert Octal 70 to Binary. (1pt)
-
+48 110000
 7.) Convert RGB (32,128,64) to Hexadecimal. (1pt)
-
+#208040
 8.) Convert 01000001, 01010011, 01000011, 01001001, 01001001 to ASCII. (1pt)
-
+A S C I I
 9.) Reached 2000 on the Binary Game? Upload screen shot!  (7pt)
 https://studio.code.org/projects/applab/iukLbcDnzqgoxuu810unLw
 
